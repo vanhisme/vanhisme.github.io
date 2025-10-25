73eaa4f25c08f07bc6d22ce975b4a6fbfc77b5a0
VietAnhDev? <112495828+vanhisme@users.noreply.github.com>
2025-10-25 12:40:33 +0700

    Delete disable-devtools.js

--- disable-devtools.js
+++
@@ -1,8 +0,0 @@
-document.onkeydown = function(e) {
-  if(e.keyCode == 123) { // F12
-    return false;
-  }
-  if(e.ctrlKey && e.shiftKey && (e.keyCode == 'I'.charCodeAt(0))) {
-    return false;
-  }
-};
